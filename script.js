cad30bcc22655999e5b7ff2a8860ad13b32346aa
Felipe <antoniofelipesilva@live.com>
2024-02-25 13:49:53 -0300

    new start code

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', function() {
+  
+    console.log('Botão de login clicado!');
+    window.alert("Clicou!!!")
+});
